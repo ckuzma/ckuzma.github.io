8d6101a369b424da7379bac260b44d8006addeb2
ckuzma <5973601+ckuzma@users.noreply.github.com>
2020-11-14 17:14:39 -0800

    Removed sweet scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,1 @@
-/* sweetScroll load */
-document.addEventListener("DOMContentLoaded", function () {
-  const sweetScroll = new SweetScroll({/* some options */});
-
-
-}, false);
+document.addEventListener("DOMContentLoaded", function () {}, false);
